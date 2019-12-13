9bf46119cc67cb2978623824d8429c02af9ff7a7
William Hickman <wahickma@unix1.csc.calpoly.edu>
2019-12-12 22:13:53 -0800

    yeee

--- DBSmoothTalker.java
+++ DBSmoothTalker.java
@@ -1,25 +1,56 @@
-import java.sql.*; 
+import java.sql.*;
+import java.util.*;
 
 public class DBSmoothTalker {
-	
-	static final String driver = "com.mysql.cj.jdbc.Driver"; 
-	static final String dburl = "jdbc:mysql://csc365.toshikuboi.net:3306/group08"; 
-	
-	static final String user = "group08"; 
-	static final String pass = "Group08@csc365"; 
-	
-	public static void main(String[] args) {
-		try {
-			Class.forName(driver); 
-			Connection connection = DriverManager.getConnection(dburl, user, pass); 
-			
-			// TODO: code shit here lol
-			
-			connection.close();
-		} catch (Exception sqlException) {
-			sqlException.printStackTrace();
-		}
-		
-	}
-	
+
+   static final String driver = "com.mysql.cj.jdbc.Driver";
+   static final String dburl = "jdbc:mysql://csc365.toshikuboi.net:3306/group08"; 
+
+   static final String user = "group08";
+   static final String pass = "Group08@csc365";
+
+   static private String first = "";
+   static private String last = "";
+   
+   static private String[] supportedCommands = {"search", "book", "cancel"};  
+
+   public static void main(String[] args) {
+      try {
+         Class.forName(driver);
+         Connection connection = DriverManager.getConnection(dburl, user, pass);
+
+         ultimateUIExperience(connection);
+
+         connection.close();
+      } catch (Exception sqlException) {
+         sqlException.printStackTrace();
+      }
+
+   }
+   
+   private static void ultimateUIExperience(Connection connection) {
+      Scanner scan = new Scanner(System.in); 
+      String input;
+      
+      System.out.println("SignIn: <Firstname> <Lastname>"); 
+      String[] si = scan.nextLine().split(" ");
+      first = si[0]; 
+      last = si[1]; 
+
+      //TODO: user signin into database
+      
+      do {
+    	  	System.out.println("Commands: Search, Book, Cancel"); 
+    	  	input = scan.nextLine(); 
+    	  	executeCommand(connection, input); 
+      } while (!input.equals("exit")); 
+   }
+   
+   private static void executeCommand(Connection connection, String command) {
+      ArrayList<String> sc = new ArrayList<String>(Arrays.asList(supportedCommands)); 
+	  if (!sc.contains(command)) {
+    	     return; 
+      }
+   }
+   
 }
