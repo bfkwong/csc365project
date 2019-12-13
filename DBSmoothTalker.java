d0eff438a9f4b16eee02ada50199bad6a03b6811
bryank458 <bryank458>
2019-12-12 22:45:23 -0800

    Sign in function

--- DBSmoothTalker.java
+++ DBSmoothTalker.java
@@ -11,7 +11,8 @@ public class DBSmoothTalker {
 
    static private String first = "";
    static private String last = "";
-   
+   static private int userId = 0;    
+
    static private String[] supportedCommands = {"search", "book", "cancel"};  
 
    public static void main(String[] args) {
@@ -37,6 +38,7 @@ public class DBSmoothTalker {
       first = si[0]; 
       last = si[1]; 
 
+      signInUser(first, last, connection); 
       //TODO: user signin into database
       
       do {
@@ -52,5 +54,28 @@ public class DBSmoothTalker {
     	     return; 
       }
    }
+
+   private static int signInUser(String first, String last, Connection connObj) {
+      PreparedStatement ps = connObj.prepareStatement("SELECT * FROM Users WHERE firstName = ? and lastName = ?"); 
+      ps.setString(1, first); 
+      ps.setString(2, last); 
+
+      ResultSet res = ps.executeQuery(); 
+
+      if (res.next() == null) {
+         PreparedStatement ps_ins = connObj.prepareStatement("INSERT INTO Users (firstName, lastName) VALUE (?, ?)");
+         ps_ins.setString(1, first); 
+         ps_ins.setString(2, first); 
+         
+         ps_ins.executeQuery(); 
+      } else {
+         res.beforeFirst(); 
+         res.next(); 
+
+         userId = res.getInt("id");
+         System.out.println(first + " " + last + " (User ID: " + userId + ") is now signed in.");      
+      }
+   }
+
    
 }
